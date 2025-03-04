e8427479972afeaa8aed8d5fb946ea2afc1a706c
Joshuams128 <Joshuams128@gmail.com>
2025-03-04 08:52:50 -0500

    activate_gutenberg_product

--- themes/no-code-theme/inc/hooks.php
+++ themes/no-code-theme/inc/hooks.php
@@ -2,7 +2,7 @@
 /**
  * enquene block editor assets
  */
-function cm2_test_01_enqueue_block_editor_asset() {
+function cm2_test_01_enqueue_block_editor_assets() {
 	wp_enqueue_script(
 		'cm2-test-01-block-editor',
 		get_theme_file_uri( 'assets/js/block-editor.js' ),
@@ -14,22 +14,14 @@ function cm2_test_01_enqueue_block_editor_asset() {
 	);
 } 
 
-add_action( 'enqueue_block_editor_assets', 'cm2_test_01_enqueue_block_editor_asset' );
+add_action( 'enqueue_block_editor_assets', 'cm2_test_01_enqueue_block_editor_assets' );
 
-/**
- * enquene block editor assets
- */
 
-function cm2_test_01_enqueue_block_styles() {
+function cm2_test_01_enqueue_block_assets() {
 	wp_enqueue_style(
-		'cm2-test-01-block-editor',
+		'cm2-test-01-block-styles',
 		get_theme_file_uri( 'assets/css/block-styles.css' ),
-		array( 
-			'wp-blocks', 
-			'wp-dom-ready', 
-			'wp-edit-post' 
-		)
 	);
 } 
 
-add_action( 'enqueue_block_assets', 'cm2_test_01_enqueue_block_asset' );
+add_action( 'enqueue_block_assets', 'cm2_test_01_enqueue_block_assets' );
