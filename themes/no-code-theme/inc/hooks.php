49ff76c69bcc665815c36146fff36a31fb9dc600
Joshuams128 <Joshuams128@gmail.com>
2025-03-03 23:38:55 -0500

    created css

--- themes/no-code-theme/inc/hooks.php
+++ themes/no-code-theme/inc/hooks.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * enquene block editor assets
+ */
 function cm2_test_01_enqueue_block_editor_asset() {
 	wp_enqueue_script(
 		'cm2-test-01-block-editor',
@@ -13,3 +15,21 @@ function cm2_test_01_enqueue_block_editor_asset() {
 } 
 
 add_action( 'enqueue_block_editor_assets', 'cm2_test_01_enqueue_block_editor_asset' );
+
+/**
+ * enquene block editor assets
+ */
+
+function cm2_test_01_enqueue_block_styles() {
+	wp_enqueue_style(
+		'cm2-test-01-block-editor',
+		get_theme_file_uri( 'assets/css/block-styles.css' ),
+		array( 
+			'wp-blocks', 
+			'wp-dom-ready', 
+			'wp-edit-post' 
+		)
+	);
+} 
+
+add_action( 'enqueue_block_assets', 'cm2_test_01_enqueue_block_asset' );
